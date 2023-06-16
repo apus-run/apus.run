cfb5d674c6f5ba0c1cffa5bb9e2c365792d84c6a
moocss <moocss@gmail.com>
2023-06-16 13:52:02 +0800

    bug: remove useModel

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -4,6 +4,8 @@ import { useLatest } from '@apus.run/react-hooks';
 function App() {
   const [count, setCount] = useState(0);
 
+
+
   const latestCountRef = useLatest(count);
 
   useEffect(() => {
